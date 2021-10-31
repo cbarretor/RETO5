d09fe2efa10fbf9cb279ba436e21dd7e6e41b5b8
Camilo Barreto <camilo.barreto.mt@correo.usa.edu.co>
2021-10-31 09:15:46 -0500

    first commit

---
+++ Reto5/src/main/java/tutoria/ClasesReto5/ContadorCliente.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tutoria.ClasesReto5;
+
+import tutoria.Modelo.Cliente;
+
+/**
+ *
+ * @author DELL
+ */
+public class ContadorCliente {
+    private Long total;
+    private Cliente client;
+
+    public ContadorCliente(Long total, Cliente client) {
+        this.total = total;
+        this.client = client;
+    }
+
+    public Long getTotal() {
+        return total;
+    }
+
+    public void setTotal(Long total) {
+        this.total = total;
+    }
+
+    public Cliente getClient() {
+        return client;
+    }
+
+    public void setClient(Cliente client) {
+        this.client = client;
+    }
+    
+    
+}
